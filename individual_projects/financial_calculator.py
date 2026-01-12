1249675bfd9a6f3486b6b28d5395a4d7a50876e8
arshsc <arsh.chowdhary@ucas-edu.net>
2026-01-12 10:51:05 -0700

    work on financial calc

--- individual_projects/financial_calculator.py
+++ individual_projects/financial_calculator.py
@@ -2,23 +2,47 @@
 
 
 # functions
+
 # starting menu
 def starting_menu():
-    choice = input("Enter the number to select an option\n\n1. Savings Time Calculator\n\n2. Compound Interest Calculator\n\n3. Budget Allocator\n\n4. Sales Price Calculator\n\n5. Tip Calculator\n\n").strip().lower()
+
+    choice = input("\nEnter the number to select an option\n\n1. Savings Time Calculator\n\n2. Compound Interest Calculator\n\n3. Budget Allocator\n\n4. Sales Price Calculator\n\n5. Tip Calculator\n\n").strip().lower()
     return choice
 
-# save for a goal
+# save for a goal FIX
 def save_goal():
-    goal_amount = float(input("\n\nWhat amount are you saving to: "))
-    contributing = int(input("\nHow often are you contributing?\n\n1. Weekly\n\n2. Monthly\n\n"))
-    amount_contributing = float(input("\nHow much are you contributing each time: "))
+    while True:
+        while True:
+            goal_amount = input("\nWhat amount are you saving to: ")
+            if goal_amount.isdigit() or goal_amount.isdecimal():
+                goal_amount = float(goal_amount)
+                break
+            elif goal_amount.isdigit() or goal_amount.isdecimal() == False:
+                print("\nInvalid Choice, Please Retry.")
+        while True:
+            contributing = input("\nHow often are you contributing?\n\n1. Weekly\n\n2. Monthly\n\n")
+            if contributing.isdigit()
+                contributing = int(goal_amount)
+                break
+            elif contributing.isdigit()== False:
+                print("\nInvalid Choice, Please Retry.")
+        while True:
+            amount_contributing = input("\nHow much are you contributing each time: ")
+            if amount_contributing.isdigit()
+                    amount_contributing = int(goal_amount)
+                    break
+                elif amount_contributing.isdigit()== False:
+                    print("\nInvalid Choice, Please Retry.")
 
-    if contributing == 1:
-        time = float(goal_amount / amount_contributing)
-        print(f"\nIt will take {time:.0f} weeks to save ${goal_amount:.2f}")
-    elif contributing == 2:
-        time = float((goal_amount / amount_contributing))
-        print(f"\nIt will take {time:.0f} months to save ${goal_amount:.2f}")
+        if contributing == 1:
+            time = float(goal_amount / amount_contributing)
+            print(f"\nIt will take {time:.0f} weeks to save ${goal_amount:.2f}")
+        elif contributing == 2:
+            time = float((goal_amount / amount_contributing))
+            print(f"\nIt will take {time:.0f} months to save ${goal_amount:.2f}")
+        else:
+            print("\nInvalid Choice, Please Retry.")
+            continue
 
 # compound interest FIX
 def compound_interest():
@@ -45,7 +69,6 @@ def budget_allocator():
             category = input(f"Category {count}:")
             categories_list.append(category)
         
-
 # sales price
 def sales_price():
     original_cost = float(input("\nHow much does the item originally cost: "))
@@ -64,15 +87,20 @@ def tip_calculator():
     print(f"The tip amount is ${tip} and your total is ${total_amount}")
 
 
-choice = starting_menu()
+# calculator loop
+while True:
+    choice = starting_menu()
 
-if choice == "1":
-    save_goal()
-elif choice == "2":
-    compound_interest()
-elif choice == "3":
-    budget_allocator()
-elif choice == "4":
-    sales_price()
-elif choice == "5":
-    tip_calculator()
+    if choice == "1":
+        save_goal()
+    elif choice == "2":
+        compound_interest()
+    elif choice == "3":
+        budget_allocator()
+    elif choice == "4":
+        sales_price()
+    elif choice == "5":
+        tip_calculator()
+    else:
+        print("\nInvalid Choice, Please Retry.")
+        continue
--- notes/lists.py
+++ notes/lists.py
@@ -1,2 +1,30 @@
 # AC 2nd Types of Lists Notes
-# A change
+
+
+# lists
+siblings = ["Alex", "Katie", "Andrew", "Tia", "Treyson", "Xavier", "Jake"]
+siblings[-1] = "DOW"
+siblings.append("Alex")
+print(siblings)
+# categories, data storage, control randomization
+# .append() .pop() .remove() . sort() .len() .clear() .copy() .index() .extend() .reverse()
+
+
+# tuples
+white = (255, 255, 255)
+#white = [1] = 0
+print(white[0])
+# unpack tuple
+red, green, blue = white
+print(red)
+# level info, start point, preset data, anything you don't want changed
+# count() index()
+
+
+# sets
+fruits = {"apple", "orange", "banana", "pineapple", "pear", "grapes", "orange"}
+fruits.remove("pineapple")
+fruits.add("raspberry")
+print(fruits)
+#character list, loot, passwords, randomization
+#.clear() .difference() .copy() .pop()
