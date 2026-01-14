60b46d535c95cc493827bca97f939b2e850a7a2b
arshsc <arsh.chowdhary@ucas-edu.net>
2026-01-14 20:41:13 +0000

    worked more onpersonal library

--- individual_projects/personal_library.py
+++ individual_projects/personal_library.py
@@ -13,14 +13,17 @@ movies_list = [
 # functions
 # main menu
 def main_menu():
-    choice = input("\n\nType the number for the action you would like to perform\n\n1. View\n\n2. Add\n\n3. Remove\n\n4. Search\n\n5. Exit\n\n")
+    choice = input("\n\nType the number for the action you would like to perform\n\n1. View Movies\n\n2. Add a Movie\n\n3. Remove a Movie\n\n4. Search for a Movie\n\n5. Exit Movie Library\n\n")
+
     return choice
 
 # view
 def view(movies_list):
     print("\n")
+
     for movie, director in movies_list:
         print(f"\n'{movie}' directed by {director}")
+
     print("\n")
 
 # add
@@ -30,15 +33,34 @@ def add(movies_list):
 
     movies_list.append((movie, director))
 
-    print(f"\nSucessfully Added\n\n'{movie}' by {director}")
+    print(f"\nSucessfully Added\n\n'{movie}' directed by {director}")
 
-# search
+# remove FIX
+def remove(movies_list):
+    view(movies_list)
 
-# remove
+    movie_remove = input("Enter the number of the corresponding move you would like to remove: ")
+
+# search FIX
+def search():
+    while True:
+        search_by = input("\n\nWhat would you like to search by?\n\n1. Movie\n\n2. Director\n\n")
+
+        if search_by == "1":
+            searching = input("\n\nWhat is the movie's name: ")
+            return searching
+        
+        elif search_by == "2":
+            searching = input("\n\nWhat is the director's name: ")
+            return searching
+        
+        else:
+            print("\n\nInvalid choice, please retry.")
 
 # main code
-
 def main(movies_list):
+    print("\nYour Movie Library with Directors")
+
     while True:
         choice = main_menu().strip()
 
@@ -47,6 +69,26 @@ def main(movies_list):
         
         elif choice == "2":
             add(movies_list)
+        
+        elif choice == "3":
+            remove(movies_list)
+        
+        elif choice == "4":
+            searching = search(movies_list)
+            print(searching)
+
+            if searching in movies_list:
+                for movie, director in movies_list:
+                    if movie == searching or director == searching:
+                        print(f"\n{movie} by {director}")
+
+
+        elif choice == "5":
+            print("\nExiting...")
+            break
+        
+        else:
+            print("\nInvalid choice, please retry.")
 
 
 main(movies_list)
