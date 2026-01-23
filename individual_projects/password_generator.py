a73af7e90300978057a36cd4b8c6e207ce549926
arshsc <arsh.chowdhary@ucas-edu.net>
2026-01-23 10:49:17 -0700

    working on more on ts password generatororrorroro

--- individual_projects/password_generator.py
+++ individual_projects/password_generator.py
@@ -1,6 +1,13 @@
 # AC 2nd Random Password Generator
 
+
+# libraries
+import random
+import string
+
+
 # variables
+length = 0
 lowercases_required = False
 uppercases_required = False
 numbers_required = False
@@ -8,10 +15,6 @@ special_characters_required = False
 
 password = []
 
-# lists
-#lowercase_letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-#uppercase_letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-#numbers = []
 
 # functions
 def intro():
@@ -21,55 +24,61 @@ def main_menu():
     choice = input("\nType the number for the action you would like to perform\n\n1. Generate Passwords\n\n2. Exit\n\n").strip()
     return choice
 
-def generate_password(lowercases_required, uppercases_required, numbers_required, special_characters_required):
-
-    def requirement(option, option_required):
-        while True:
-            required = input(f"\nDoes the password need {option} letters (Y/N): ").strip().upper()
-
-            if required == "Y" or "N":
-                if required == "Y":
-                    option_required = True
-                    return option_required
-                elif required == "N":
-                    option_required = False
-                    return option_required
-                break
-            else:
-                print("\nInvalid choice, please retry.")
-            
+def requirement(prompt, option_required):
     while True:
-        length = input("\nHow long does the password need to be: ").strip()
-        if length.isdigit():
-            return length
-        elif length.isdigit() == False:
+        required = input(f"\n{prompt}").strip().upper()
+
+        if required == "Y" or "N":
+            if required == "Y":
+                option_required = True
+                return option_required
+            elif required == "N":
+                option_required = False
+                return option_required
+            break
+        else:
             print("\nInvalid choice, please retry.")
-    
-    requirement("lowercase", lowercases_required)
-    requirement("uppercase", uppercases_required)
-    requirement("number", numbers_required)
-    requirement("special_character", special_characters_required)
 
-def length(password, length):
-    if length == True:
-        password = ["X"] * 5
-        return password
+def password_length(password, length):
+    for i in range(length):
+        random_number = random.randint(65, 90)
+        random_letter = chr(random_number)
+        password.append(random_letter)
+    return password
 
 
-def main(lowercases_required, uppercases_required, numbers_required, special_characters_required, password, length):
+def main(password, length, lowercases_required = False, uppercases_required = False, numbers_required = False, special_characters_required = False):
+
+
     intro()
 
     while True:
+        print(password)
+        print(length, lowercases_required, uppercases_required, numbers_required, special_characters_required)
 
         choice = main_menu()
 
         if choice == "1":
-            generate_password(lowercases_required, uppercases_required, numbers_required, special_characters_required)
-            length()
+            while True:
+                length = input("\nHow long does the password need to be: ").strip()
+                if length.isdigit():
+                    length = int(length)
+                    break
+                elif length.isdigit() == False:
+                    print("\nInvalid choice, please retry.")
+            lowercases_required = requirement("Does the password need lowercase letters (Y/N): ", lowercases_required)
+            uppercases_required = requirement("Does the password need uppercase letters (Y/N): ", uppercases_required)
+            numbers_required = requirement("Does the password need numbers (Y/N): ", numbers_required)
+            special_characters_required = requirement("Does the password need special characters (Y/N): ", special_characters_required)
+
+            password = password_length(password, length)
+
         elif choice == "2":
             print("\nExiting...")
+
         else:
             print("\nInvalid choice, please retry.")
 
 
-main(lowercases_required, uppercases_required, numbers_required, special_characters_required, password, length)
+
+main(password, length, lowercases_required, uppercases_required, numbers_required, special_characters_required)
