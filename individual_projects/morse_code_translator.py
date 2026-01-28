34cd0c6e0f3dddc6705a62764dbed81ac4d34d8a
arshsc <arsh.chowdhary@ucas-edu.net>
2026-01-28 10:50:54 -0700

    worked on morese code

---
+++ individual_projects/morse_code_translator.py
@@ -0,0 +1,15 @@
+# AC 2nd Simple Morse Code Translator
+
+def main_menu():
+    choice = input("\n1. Translate from Morse code to English\n\n2. Translate from English to Morse Code\n\n3. Exit\n\n")
+    return choice
+
+def english_morse_code():
+    pass
+
+def morse_code_english():
+    pass
+
+def main():
+    choice = main_menu()
+
