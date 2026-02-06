274147a0006f2840dcd291d35fd139f639fe935c
arshsc <arsh.chowdhary@ucas-edu.net>
2026-02-06 10:51:37 -0700

    worked on movie recc

--- individual_projects/movie_recommender.py
+++ individual_projects/movie_recommender.py
@@ -11,51 +11,54 @@ def main_menu():
 
 def search():
     filters = input("\nChoose filters to apply (enter numbers seperated by commas, e.g., '1,3')\n\n1. Genre\n2. Director\n3. Actor\n4. Length (min/max)\n\nSelected Filters: ")
+    if filters == "1":
+        genre_search()
 
-    def genre_search():
-        genre = input("\nEnter genre (e.g., \"Science Fiction\"): ")
 
-    def director_search():
-        director = input("\nEnter director name: ")
+def genre_search():
+    genre = input("\nEnter genre (e.g., \"Science Fiction\"): ").title()
+    with open("individual_projects\movie_list.csv", "r") as file:
+        reader = csv.reader(file)
+        found = False
+        for row in reader:
+            if len(row > 1 and row[1] == genre):
+                print(f"found in this row: {row}")
+                found = True
+        if not found:
+            print("not found")
 
-    def actor_search():
-        actor = input("\nEnter actor name: ")
+def director_search():
+    director = input("\nEnter director name: ")
 
-    def length_search():
-        min_length = input("\nEnter minumum length in minutes (or leave blank): ")
-        max_length = input("Enter maximum length in minutes (or leave blank): ")
+def actor_search():
+    actor = input("\nEnter actor name: ")
+
+def length_search():
+    min_length = input("\nEnter minumum length in minutes (or leave blank): ")
+    max_length = input("Enter maximum length in minutes (or leave blank): ")
 
 def print_movies():
-    try:
-        with open("individual_projects\movie_list.csv", mode= "r") as file:
+    try: 
+        with open("individual_projects\movie_list.csv", mode='r', newline='') as file:
             reader = csv.reader(file)
-            header = next(reader)
-            movies = []
-            for line in reader:
-                movies.append(
-                    {
-                        header[0]: line[0],
-                        header[1]: line[1],
-                        header[2]: line[2],
-                        header[3]: line[3],
-                        header[4]: line[4],
-                        header[5]: line[5],
-                    }
-                )
+            
+            for row in reader:
+                if len(row) > max([0, 2], default=-1):
+                    print(f"\nTitle: \"{row[[0, 0][0]]}\" -- Director: {row[[0, 1][1]]} -- Genre: {row[[0, 2][1]]} -- Rating: {row[[0, 3][1]]} -- Length: {row[[0, 4][1]]} min -- Notable Actors: {row[[0, 5][1]]}")
     except:
-        print("Can't find CSV")
-    else:
-        for movie in movies:
-            print(f"{header[0]}: {movie}")
+        print("Can't find that file")
 
 def main():
     intro()
     while True:
         choice = main_menu()
         if choice == "1":
-            break
-        if choice == "2":
+            search()
+        elif choice == "2":
             print_movies()
+        elif choice == "3":
+            print("\nExiting")
+            break
 
 
 main()
