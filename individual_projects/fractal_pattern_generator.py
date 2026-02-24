54bb45a08d70cdcb63dc1b9623ccebd4c768b714
arshsc <arsh.chowdhary@ucas-edu.net>
2026-02-24 10:51:29 -0700

    recursion notes and thgen madefractal file

---
+++ individual_projects/fractal_pattern_generator.py
@@ -0,0 +1,8 @@
+# AC 2nd Fractal Pattern Generator
+
+import turtle
+
+def intro():
+    print("Welcome to the Sierpinski Triangle Generator! This program creates a Sierpinksi Triangle fractal using recursion.")
+
+def sierpinski_triangle(n):
---
+++ notes/recursion.py
@@ -0,0 +1,43 @@
+# AC 2nd Recursion Notes
+
+number = 5
+factorial = 1
+
+while number > 0:
+    factorial *= number
+    number -=1
+
+print(factorial)
+
+def factor(num):
+    if num == 1: return 1
+    return num * factor(num-1)
+
+print(factor(5))
+
+
+number = 10
+sequence = [1, 1]
+for i in range(1, number):
+    sequence.append(sequence[i] + sequence[i-1])
+
+print(sequence)
+
+recursive_sequence = [1,1]
+"""def fibonacci(n):
+    if n == 1:
+        return 0
+    elif n == 2:
+        return 1
+    else:
+        recursive_sequence.append(recursive_sequence[fibonacci(n)] + recursive_sequence[n-1])"""
+
+def fibonacci(n):
+    if n == 1:
+        return 1
+    elif n == 2:
+        return 1
+    else:
+        recursive_sequence.append(recursive_sequence[fibonacci(n-2)])
+
+    print(fibonacci(10))
