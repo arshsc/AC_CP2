c88ac16168ea2f8a4e71f9e33b628fa6fb7eb416
arshsc <arsh.chowdhary@ucas-edu.net>
2026-03-27 10:50:24 -0600

    did some stuff for gui

--- notes/guiproject.py
+++ notes/guiproject.py
@@ -1,14 +1,29 @@
 from tkinter import *
-from tkinter import ttk
+import tkinter as tk
 
 root = Tk()
 root.title("Test GUI")
 root.geometry("600x600")
 
-label = Label(root,text="Test GUI LABEL", bg="red", fg="white", font=("Helvetica", 20))
-label.pack()
+def button_blicked():
+    print("Button clicked!")
+
+def main_menu():
+    label = Label(root,text="Test GUI LABEL", bg="red", fg="white", font=("Helvetica", 40), width=50, height=3)
+    label.pack(padx=20, pady=5, anchor="center")
+
+    first_button = tk.Button(root, text="First Button", command=button_blicked, width=25, height=2, font=("Arial", 12), bd=5, bg="white", activebackground="grey", activeforeground="white", overrelief="solid")
+    second_button = tk.Button(root, text="Second Button", command=button_blicked, width=25, height=2, font=("Arial", 12), bd=5, bg="white", activebackground="grey", activeforeground="white", overrelief="solid")
+    third_button = tk.Button(root, text="Third Button", command=button_blicked, width=25, height=2, font=("Arial", 12), bd=5, bg="white", activebackground="grey", activeforeground="white", overrelief="solid")
+    fourth_button = tk.Button(root, text="Fourth Button", command=button_blicked, width=25, height=2, font=("Arial", 12), bd=5, bg="white", activebackground="grey", activeforeground="white", overrelief="solid")
+
+    # 25 pixels away
+    first_button.pack(padx=20, pady=5, anchor="center",)
+    second_button.pack(padx=20, pady=5, anchor="center")
+    third_button.pack(padx=20, pady=5, anchor="center")
+    fourth_button.pack(padx=20, pady=5, anchor="center")
 
 
-
+main_menu()
 
 root.mainloop()
