22a573b175416c2b585f1510ea3a77370856d6bd
arshsc <arsh.chowdhary@ucas-edu.net>
2026-04-23 10:50:23 -0600

    pygame learning

---
+++ notes/pygame_test.py
@@ -0,0 +1,57 @@
+"""
+pygame-menu
+https://github.com/ppizarror/pygame-menu
+
+EXAMPLE - SIMPLE
+Super simple example of pygame-menu usage, featuring a selector and a button.
+"""
+
+from __future__ import annotations
+
+from typing import Any
+
+import pygame_menu
+from pygame_menu.examples import create_example_window
+
+surface = create_example_window("Example - Simple", (600, 400))
+
+
+def set_difficulty(selected: tuple, value: Any) -> None:
+    """
+    Set the difficulty of the game.
+    """
+    print(f"Set difficulty to {selected[0]} ({value})")
+
+
+def start_the_game() -> None:
+    """
+    Function that starts a game. This is raised by the menu button,
+    here menu can be disabled, etc.
+    """
+    global user_name
+    saves_menu
+
+def saves_menu():
+    menu = pygame_menu.Menu(
+        height=300, theme=pygame_menu.themes.THEME_BLUE, title="Saves Menu", width=400
+    )
+
+    user_name = menu.add.text_input("Name: ", default="John Doe", maxchar=10)
+    menu.add.selector("Difficulty: ", [("Hard", 1), ("Easy", 2)], onchange=set_difficulty)
+    menu.add.button("Play", start_the_game)
+    menu.add.button("Quit", pygame_menu.events.EXIT)
+
+def main_menu():
+    menu = pygame_menu.Menu(
+        height=300, theme=pygame_menu.themes.THEME_BLUE, title="Welcome", width=400
+    )
+
+    user_name = menu.add.text_input("Name: ", default="John Doe", maxchar=10)
+    menu.add.selector("Difficulty: ", [("Hard", 1), ("Easy", 2)], onchange=set_difficulty)
+    menu.add.button("Play", start_the_game)
+    menu.add.button("Quit", pygame_menu.events.EXIT)
+
+    if __name__ == "__main__":
+        menu.mainloop(surface)
+
+main_menu()
